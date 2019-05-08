2b7d00cf4c82eea940c8cef5dcd742e52f27eef1
Erik Forsberg <erikforsberg95@hotmail.com>
2019-05-08 15:24:41 +0200

    Create StepForwardWrapping.py

---
+++ StepForwardWrapping.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 15:10:49 2019
+
+@author: ErikF
+"""
+
+import numpy as np
+import sklearn as sk
+
+def StepForrwardWrapping(data, labels, nrFeatures): 
+    nrClasses = labels.shape[1]
+    nrFeaturesOriginal = X.shape[1]
+    nrDataPoints = X.shape[0]
+    features = []
+    for i in range(nrFeatures):
+        for j in range(nrFeaturesOriginal):
+            
+            
+        
+    
