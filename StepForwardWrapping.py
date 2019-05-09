350c939b2531015aec7ad9955e78418e1a00587b
Erik Forsberg <erikforsberg95@hotmail.com>
2019-05-09 13:28:31 +0200

    continued on step forward

--- StepForwardWrapping.py
+++ StepForwardWrapping.py
@@ -6,16 +6,17 @@ Created on Wed May  8 15:10:49 2019
 """
 
 import numpy as np
-import sklearn as sk
+import sklearn.neighbors as sk
 
-def StepForrwardWrapping(data, labels, nrFeatures): 
-    nrClasses = labels.shape[1]
-    nrFeaturesOriginal = X.shape[1]
-    nrDataPoints = X.shape[0]
+def StepForwardWrapping(data, labels, nrFeatures, k = 5): 
+    nrClasses = len(set(labels))
+    nrDataPts, nrFeaturesOriginal = data.shape
+   # nrDataPoints = data.shape[0]
     features = []
     for i in range(nrFeatures):
         for j in range(nrFeaturesOriginal):
+            sk.KNeighborsClassifier(n_neighbours = k).fit(data,labels)
             
-            
+
+            return data
         
-    
